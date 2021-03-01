684015325d7849c5295306c5e9ac703b5d605934
nazk420testing <76122083+nazk420testing@users.noreply.github.com>
2021-03-01 16:28:23 +0000

    Initial commit

---
+++ src/main/java/com/nz/BaseJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nz/BaseJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
